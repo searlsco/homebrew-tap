e51dc7e6ed3861b4c8445bb5bcfa7090216ac0db
bitsly <208552577+Bitsly@users.noreply.github.com>
2025-09-23 15:14:28 +0000

    autochapter v0.0.1: bump formula

--- Formula/autochapter.rb
+++ Formula/autochapter.rb
@@ -1,8 +1,8 @@
 class Autochapter < Formula
   desc "Automatically add podcast chapters by detecting stereo audio stingers"
   homepage "https://github.com/searlsco/autochapter"
-  url "https://github.com/searlsco/autochapter/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "REPLACE_WITH_REAL_SHA256"
+  url "https://github.com/searlsco/autochapter/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "acc1e2cfb5bf454243626c5b7d38fa70b5f38ba89b04b06340b044f7ebe3cda2"
   license "MIT"
 
   depends_on "ffmpeg"
