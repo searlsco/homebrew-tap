f8aa467f764a44262d578c7547c95f86011ec90d
bitsly <208552577+Bitsly@users.noreply.github.com>
2026-02-01 19:24:49 +0000

    scrapple v0.0.2: bump formula

--- Formula/scrapple.rb
+++ Formula/scrapple.rb
@@ -1,8 +1,8 @@
 class Scrapple < Formula
   desc "Local Apple Developer Documentation scraper and search tool"
   homepage "https://github.com/searlsco/scrapple"
-  url "https://github.com/searlsco/scrapple/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "4a8cd21d18501ebab4460b3c57d233fa849e9df0efe1f26b8f4075fbc2dd1c86"
+  url "https://github.com/searlsco/scrapple/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "4d1c7369ff5b87ffc065505e237ff88d315352c141b2bf12fc2cf0282ac8e8b5"
   license "MIT"
 
   depends_on "node"
